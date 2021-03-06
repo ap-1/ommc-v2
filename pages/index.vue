6aa3c934c81f730a592b05f3ae21bc0b7e58ce60
EmperorParzival <29981946+EmperorParzival@users.noreply.github.com>
2021-03-05 21:16:42 -0500

    feat: Restructure home page and add icon

--- pages/index.vue
+++ pages/index.vue
@@ -1,97 +1,22 @@
 <template>
   <main>
-    <client-only>
-      <Particles
-        id="particles"
-        :options="{
-          fpsLimit: 60,
-          particles: {
-            number: {
-              value: 80,
-              density: {
-                enable: true,
-                value_area: 800,
-              },
-            },
-            color: {
-              value: '#ffffff',
-            },
-            shape: {
-              type: 'circle',
-              stroke: {
-                width: 0,
-                color: '#000000',
-              },
-            },
-            opacity: {
-              value: 0.5,
-              random: true,
-              anim: {
-                enable: true,
-                speed: 0.2,
-                opacity_min: 0.2,
-                sync: false,
-              },
-            },
-            size: {
-              value: 3,
-              random: true,
-            },
-            move: {
-              enable: true,
-              speed: 1,
-              direction: 'top-right',
-              random: true,
-              straight: true,
-              out_mode: 'out',
-              bounce: false,
-            },
-          },
-          interactivity: {
-            detect_on: 'window',
-            events: {
-              onhover: {
-                enable: true,
-                mode: 'grab',
-              },
-              onclick: {
-                enable: true,
-                mode: 'repulse',
-              },
-              resize: true,
-            },
-            modes: {
-              grab: {
-                distance: 125,
-                line_linked: {
-                  opacity: 1,
-                },
-              },
-              repulse: {
-                distance: 225,
-                duration: 0.4,
-              },
-            },
-          },
-          retina_detect: true,
-        }"
-      />
-    </client-only>
-
-    <section class="hero is-primary is-bold is-fullheight mb-6">
+    <section class="hero is-dark is-bold is-fullheight mb-6">
       <div class="hero-head">
-        <navbar :id="id" :embed="true" />
+        <Navbar />
       </div>
 
       <div class="hero-body">
         <div class="container has-text-centered">
-          <p class="brand is-uppercase has-text-weight-light" no-select>ommc</p>
-          <p
-            class="subtitle is-capitalized is-size-4 has-text-weight-light"
-            no-select
-          >
-            online monmouth math competition
-          </p>
+          <!--
+            <p class="brand is-uppercase has-text-weight-light" no-select>ommc</p>
+            <p
+              class="subtitle is-capitalized is-size-4 has-text-weight-light"
+              no-select
+            >
+              online monmouth math competition
+            </p>
+          -->
+          <img src="~/assets/icon.png" alt="OMMC" />
         </div>
       </div>
 
@@ -102,48 +27,46 @@
       </div>
     </section>
 
-    <fade-in title="intro">
+    <FadeIn title="intro">
       <template #content>
-        <div class="column is-half">
-          <p class="title has-text-weight-light">Ready to join?</p>
-          <p class="subtitle is-size-4 has-text-weight-light">
-            Interact with the community and form teams on our discord.
-          </p>
-        </div>
+        <div class="columns is-centered">
+          <div class="column">
+            <p class="title has-text-white has-text-weight-light">
+              Ready to join?
+            </p>
+            <p class="subtitle has-text-white is-size-4 has-text-weight-light">
+              Interact with the community and form teams on our discord.
+            </p>
+          </div>
 
-        <div class="column is-half">
-          <iframe
-            width="350"
-            height="500"
-            frameborder="0"
-            class="is-pulled-right"
-            allowtransparency="true"
-            src="//discord.com/widget?id=796756256022200350&amp;theme=dark"
-            sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
-          >
-          </iframe>
+          <div class="column">
+            <iframe
+              width="350"
+              height="500"
+              frameborder="0"
+              class="is-pulled-right"
+              allowtransparency="true"
+              src="//discord.com/widget?id=796756256022200350&amp;theme=dark"
+              sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+            >
+            </iframe>
+          </div>
         </div>
       </template>
-    </fade-in>
+    </FadeIn>
   </main>
 </template>
 
-<script>
+<script lang="ts">
 import Vue from 'vue'
 
-import navbar from '../components/navbar.vue'
-import fadeIn from '../components/fade-in.vue'
+import Navbar from '../components/Navbar.vue'
+import FadeIn from '../components/FadeIn.vue'
 
 export default Vue.extend({
   name: 'Home',
   layout: 'landing',
-  components: { navbar, fadeIn },
-
-  data() {
-    return {
-      id: 'prelim-nav',
-    }
-  },
+  components: { Navbar, FadeIn },
 })
 </script>
 
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,13 +1,9 @@
 import { shallowMount } from '@vue/test-utils'
 
 import index from '@/pages/index.vue'
-import Navbar from '~/components/Navbar.vue'
 
 describe('index', () => {
   const wrapper = shallowMount(index)
 
   it('renders successfully', () => expect(wrapper.vm).toBeTruthy())
-
-  it('renders the navbar', () =>
-    expect(wrapper.findComponent(Navbar).exists()).toBe(true))
 })
